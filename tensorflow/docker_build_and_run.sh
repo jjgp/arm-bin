7339c10e32f2b3bc23e144eb6b560c43311682d9
Jason Prasad <jasongprasad@gmail.com>
2021-07-28 09:24:28 +0100

    Remove ENTRYPOINT in favor of using args to docker run

--- tensorflow/docker_build_and_run.sh
+++ tensorflow/docker_build_and_run.sh
@@ -4,6 +4,10 @@ set -eu
 
 docker build -t arm-pip-tensorflow .
 
-docker run -v $PWD/bin/:/mnt \
+docker run -w /tensorflow \
+    -v $PWD/build_from_source.sh:/build_from_source.sh \
+    -v $PWD/bin/:/mnt \
     -e HOST_PERMS="$(id -u):$(id -g)" \
-    arm-pip-tensorflow
+    arm-pip-tensorflow \
+    /bin/bash \
+    /build_from_source.sh
